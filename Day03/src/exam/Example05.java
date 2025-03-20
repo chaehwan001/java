17613e275f7cb7547db3bedb2b157990a74f0415
chaehwan001 <coghks5899@gmail.com>
2025-03-20 21:45:38 +0900

    Add files via upload

---
+++ Day03/src/exam/Example05.java
@@ -0,0 +1,24 @@
+package exam;
+
+import java.util.Scanner;
+
+public class Example05 {
+	public static void main(String[] args) {
+		/*
+		 * Scanner()란 사용자가 데이터를 입력받을 때 사용하는 클래스
+		 * 
+		 * 형식) Scanner sc = new Scanner(System.in);    (sc는 바꿔도 됨)
+		 * 
+		 * sc.nextByte() : byte형 입력 및 리턴
+		 * sc.nextShort() : short형 입력 및 리턴
+		 * sc.nextInt(): int형 입력 및 리턴
+		 * sc.nextLong(): long형 입력 및 리턴
+		 * 
+		 * sc.nextFloat(): float형 입력 및 리턴
+		 * sc.nextDouble(): double형 입력 및 리턴
+		 * 
+		 * sc.next() : string형 입력 및 리턴
+		 * sc.nextLine(): string형 입력 및 리턴
+		 */
+	}
+}
